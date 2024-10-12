ef64dbca42d16d82dc9f3fbad4d7ad340d0da577
yasithabhanuka <yasithabhanukac@gmail.com>
2024-10-12 08:39:27 +0530

    Make the future void function loadConfig for before the app start, App should read the main.json file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:valuecoin/pages/home_page.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await loadConfig();
   runApp(const MyApp());
+}
+
+Future<void> loadConfig() async {
+  String _configContent =
+      await rootBundle.loadString('assets/config/main.json');
+
+  Map _configData = jsonDecode(_configContent);
+  // print(_configContent);
+  print(_configData);
 }
 
 class MyApp extends StatelessWidget {
