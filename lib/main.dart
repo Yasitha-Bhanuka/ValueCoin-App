554d315025433d4a025b5108ffc3224b28e5594d
yasithabhanuka <yasithabhanukac@gmail.com>
2024-10-12 08:46:48 +0530

    Using get_it package create the instance from the using app_config.dart class.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
+import 'package:valuecoin/models/app_config.dart';
 import 'package:valuecoin/pages/home_page.dart';
 
 void main() async {
@@ -15,8 +17,12 @@ Future<void> loadConfig() async {
       await rootBundle.loadString('assets/config/main.json');
 
   Map _configData = jsonDecode(_configContent);
-  // print(_configContent);
   print(_configData);
+  GetIt.instance.registerSingleton<AppConfig>(
+    AppConfig(
+      COIN_API_BASE_URL: _configData['COIN_API_BASE_URL'],
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
