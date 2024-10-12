178ac1d9cfce4488f224520e55aeb53025ff6a35
yasithabhanuka <yasithabhanukac@gmail.com>
2024-10-12 08:28:53 +0530

    All the native files binding with the flutter app using WidgetsFlutterBinding.ensureInitialized(); inside the main function top.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:valuecoin/pages/home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
